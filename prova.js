87d8598d1ebe7ff67b444332f6816ba574efa0f3
jacoporm1989 <jgianfranchi89@icloud.com>
2022-07-29 12:59:05 +0200

    show

---
+++ prova.js
@@ -0,0 +1,31 @@
+persona1={
+    nome:'Tote',
+    cognome:'George',
+    luogoDiNascita:'Rio'
+}
+
+persona2={
+    nome:'Amilcare',
+    cognome:'Genovesi',
+    luogoDiNascita:'Torino'
+}
+
+
+persona1={
+    nome:'Guendalina',
+    cognome:'Panini',
+    luogoDiNascita:'Brescia'
+}
+
+calciatore1={
+    nome:'Tote',
+    cognome:'George'
+}
+
+salari=[{salario:1500},
+       {salario:800}
+]
+
+console.log(salari[0].salario);
+
+
